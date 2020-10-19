f523ebb597b509819f9defb83fe98634ad681c66
BlazPocrnja <bpocrnja@gmail.com>
2020-10-18 22:17:45 -0600

    Changed default IDE + some pseudo code

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,21 +4,56 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    SetupGame();
+
+    PrintLine(TEXT("The HiddenWord is: %s."), *HiddenWord); // Debug line
+    
+    // Welcoming The Player
     PrintLine(TEXT("Welcome to Bull Cows!"));
-    PrintLine(TEXT("Guess the 4 letter word!")); // Magic number remove!
-    PrintLine(TEXT("Press enter to continue..."));
-    HiddenWord = TEXT("cake");
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len());
+    PrintLine(TEXT("Type in your guess and press enter to continue..."));
+
+    // Prompt player for guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
+
+    // Checking player guess
+
     if (Input == HiddenWord)
     {
         PrintLine(TEXT("You have won!"));
     }
     else
     {
+        if (Input.Len() != HiddenWord.Len())
+        {
+            PrintLine(TEXT("The Hidden Word is %i characters Long, try again!"), HiddenWord.Len());
+        }
+        
         PrintLine(TEXT("You have lost!"));
     }
+
+    // Check if isogram
+    // Prompt to Guess Again
+    // Check if right number of characters
+    // Prompt to Guess Again
+
+    // Remove Life
+
+    // Check if Lives > 0
+    // If yes GuessAgain
+    // Show lives left
+    // If no show GameOver and HiddenWord?
+    // Prompt to play again, Press enter to play again?
+    // PlayAgain or Quit
+}
+
+void UBullCowCartridge::SetupGame() // When the player hits enter
+{
+    HiddenWord = TEXT("cake");
+    Lives = 4;
 }
